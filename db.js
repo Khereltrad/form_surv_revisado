bfaf12fa7bd65b447d6844da6351bea41c8137ae
Claudio René Aránguiz Herrera <claudioarnguiz@yahoo.com>
2021-03-01 13:08:03 -0300

    index.ejs funcional y MySQL toma datos

--- db.js
+++ db.js
@@ -1,18 +1,39 @@
 const Sequelize = require('sequelize');
-const sql = new Sequelize('zo-squet', 'root', '', { host: 'localhost', dialect: 'mysql' });
+const sql = new Sequelize('simplsurv', 'root', 'M11.n1.5am', { host: 'localhost', dialect: 'mysql' });
 
-const Mensaje = sql.define('User', {
+const Mensaje = sql.define('fichas', {
   id: {
     type: Sequelize.INTEGER,
     primaryKey: true,
     autoIncrement: true
   },
-  
-  message: {
-    type: Sequelize.TEXT,
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notNull: {
+        msg: 'Debe indicar un nombre'
+      },
+      len: {
+        args: [4],
+        msg: 'El nombre debe ser de largo al menos 4'
+      }
+    }
+  },
+  region:{
+    type: Sequelize.STRING,
     allowNull: false,
   },
-});
+  lenguaje:{
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  message: {
+    type: Sequelize.TEXT,
+    allowNull: true,
+  },
+},{ timestramps: true });
+
 
 sql.sync().then(() => {
   console.log('Base de datos y tablas creadas');
--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,21 @@
 
 const { Router } = require('express');
 const { Mensaje } = require('../db');
-// const flash = require('connect-flash');
 const router = Router();
 
 router
-  .get('/', async (req, res) => { res.render('index.ejs'); });
-  // .get('/login',checkLogin,async(req,res)=>{res.render('login.ejs');})
-  // .get('/admin',[checkLogin,checkAdmin],async(req,res)=>{res.render('admin.ejs');})
-  // .use(flash());
+  .get('/', async (req, res) => { res.render('index.ejs'); })
+  .get('/result', async (req, res) => { res.render('results.ejs'); });
+
+router
+  .get('/test', async (req, res) => {
+      const fichas = await Mensaje.findAll();
+      res.render('results.ejs',{fichas: fichas}); })
+  
+  .post('/test', async (req, res) => {
+    console.log(req.body);
+    const new_ficha = await Mensaje.create(req.body);
+    res.redirect('/');
+  });
 
 module.exports = router;
